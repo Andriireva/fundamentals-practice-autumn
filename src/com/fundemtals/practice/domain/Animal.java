847e48fa7a2b8af22ac39aadf7bc61e6a945727c
andrii.reva <andrii.reva@petabyte.technology>
2022-11-08 09:52:03 +0200

    Encapsulation, getters, setters, constructor

--- src/com/fundemtals/practice/domain/Animal.java
+++ src/com/fundemtals/practice/domain/Animal.java
@@ -5,12 +5,65 @@ public class Animal {
     // We can declare a class variable
     // We can declare a constructor
     // we can declare a method
-
     // Variables are something that explain a state of current instance of this class
-    public String name; // This variable will be used as name of particular Animal
-    public int age; // it is age of particular Animal
-    public Integer legsNumber; // Integer is wrapper to int
-    public Long childrenNumber; // example of long wrapper
+    // It is best practice making ALL field private and add setter and/or getters
+    private String name; // This variable will be used as name of particular Animal
+    private int age; // it is age of particular Animal
+    private Integer legsNumber; // Integer is wrapper to int
+    private Long childrenNumber; // example of long wrapper
+
+    // There are 4 access types public, private .....
+    // Private access means that this variable (or method.. ) it can be USED only internally
+    private Boolean hasWool; // Internal property, that should be exposed
+
+    // if we want to access to private variables, it is required to make "access" methods
+
+    // it is setter method that set the value
+    public void setHasWool(Boolean hasWool) {
+        this.hasWool = hasWool; // this keyword is a reference to instance of this class
+        System.out.println("this.hasWool value is " + this.hasWool);
+    }
+
+    public Boolean getHasWool() {
+        if (hasWool != null) {
+            return this.hasWool;
+        } else {
+            return false;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Integer getLegsNumber() {
+        return legsNumber;
+    }
+
+    // We don't want to have an ability to change legsNumber value
+//    public void setLegsNumber(Integer legsNumber) {
+//        this.legsNumber = legsNumber;
+//    }
+
+    public Long getChildrenNumber() {
+        return childrenNumber;
+    }
+
+    public void setChildrenNumber(Long childrenNumber) {
+        this.childrenNumber = childrenNumber;
+    }
 
     // primitives are fields that are mandatory
     // wrappers are fields that are optional
@@ -27,6 +80,25 @@ public class Animal {
     //  does not have "additional" methods compare to Wrappers
     // primities cannot point to "nowhere", to null
 
+    // Constructor that takes no arguments it is a default constructor
+    // If Class does not directly declare default constructor, java during compilation add it
+    // if non-default constructor is added, then default constructor will not be automatically added
+    public Animal() {
+    }
+
+    // it is a constructor, it does not have return type,
+    // and name is the same as class name
+    public Animal(Integer legNumber) {
+        this.legsNumber = legNumber;
+    }
+
+    public Animal(String name, int age, Integer legsNumber, Long childrenNumber, Boolean hasWool) {
+        this.name = name;
+        this.age = age;
+        this.legsNumber = legsNumber;
+        this.childrenNumber = childrenNumber;
+        this.hasWool = hasWool;
+    }
 
     //First Method
     public String getAnimalRepresentation() {
@@ -36,6 +108,9 @@ public class Animal {
         }
         if (childrenNumber != null) {
             representationResult = representationResult + " " + childrenNumber;
+        }
+        if (hasWool != null) {
+            representationResult = representationResult + " " + hasWool;
         }
         return representationResult;
     }
--- src/com/fundemtals/practice/domain/Zoo.java
+++ src/com/fundemtals/practice/domain/Zoo.java
@@ -2,56 +2,65 @@ package com.fundemtals.practice.domain;
 
 public class Zoo {
     public static void main(String[] args) {
-        Animal dog = new Animal(); // it creates a new instance of Animal class, and allocates new space in memory
-        Animal cat = new Animal();
-        dog.age = 11;
-        dog.name = "Rex";
-        dog.legsNumber = 6; // it is possible to init Wrapper with its primitive type
-        dog.childrenNumber = 10L;
+        Animal dog = new Animal(8); // it creates a new instance of Animal class, and allocates new space in memory
+        Animal cat = new Animal(); // we want to assign legs number ONLY 1 time
+        dog.setAge(11);
+        dog.setName("Rex");
+        //dog.setLegsNumber(6); // it is possible to init Wrapper with its primitive type
+        //dog.setLegsNumber(7); // it is possible to init Wrapper with its primitive type
+        dog.setChildrenNumber(10L);
 
-        cat.name = "Snow";
-        cat.age = 4;
+        cat.setName("Snow");
+        cat.setAge(4);
 
-        Animal cow = new Animal();
+        Animal cow = new Animal(4);
 //        cow.age = 3; // = is a reference to space in memory
         // there was no cow.name = to something called
         // When there is no assignment ( using = ) reference point to "nowhere"
         // "nowhere" is null
         // null is keyword
-        cow.name = null;
+        cow.setName(null);
 //        cow.age;
 
         int k = 5;
 
-        System.out.println("Animal name " + dog.name);
-        System.out.println("Animal age " + dog.age);
+        System.out.println("Animal name " + dog.getName());
+        System.out.println("Animal age " + dog.getAge());
 
-        System.out.println("Animal name " + cat.name);
-        System.out.println("Animal age " + cat.age);
+        System.out.println("Animal name " + cat.getName());
+        System.out.println("Animal age " + cat.getAge());
 
-        System.out.println("Cow age is " + cow.age);
-        System.out.println("Cow name is " + cow.name);
+        System.out.println("Cow age is " + cow.getAge());
+        System.out.println("Cow name is " + cow.getName());
 
         Animal[] animals = {dog, cat, cow};
 
         // dog and animals[0] has pointer to the same place in memory
-        dog.age = 7;
-        System.out.println("Animal age " + dog.age);
-        dog.age = 16;
-        System.out.println("First animal age is " + animals[0].age);
+        dog.setAge(7);
+        System.out.println("Animal age " + dog.getAge());
+        dog.setAge(16);
+        System.out.println("First animal age is " + animals[0].getAge());
 
         Animal dog2 = dog;
-        Animal dog3 = new Animal();
-        dog3.name = "RexXXXX";
-        dog3.age = 16;
+        Animal dog3 = new Animal(4);
+        dog3.setName("RexXXXX");
+        dog3.setAge(16);
 
-        System.out.println("Dog 2 age  is " + dog2.age);
+        System.out.println("Dog 2 age  is " + dog2.getAge());
         System.out.println("Compare dog and dog2 " + (dog == dog2));
         System.out.println("Compare dog2 and dog3 " + (dog2 == dog3));
 
-        String animalRepresentation = dog.getAnimalRepresentation();
+        String animalRepresentation = dog.getAnimalRepresentation(); // at current time
+        dog.setHasWool(true);
+        cat.setHasWool(false);
+        System.out.println("Dog has wool is " + dog.getHasWool());
+        System.out.println("Cat has wool is " + cat.getHasWool());
+        System.out.println("Cow has wool is " + cow.getHasWool());
+
         System.out.println("dog representation is " + animalRepresentation);
         System.out.println("Cat representation is " + cat.getAnimalRepresentation());
 
+        Animal elephant = new Animal("Dambo", 1, 4, 0L, false);
+        System.out.println("elephant representation is " + elephant.getAnimalRepresentation());
     }
 }
